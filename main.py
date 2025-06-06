c6771c6619af4fb8caf31dec9fb14a021c45fbfe
김승용 <gkstjeo17@naver.com>
2025-06-06 23:36:46 +0900

    refactor: analyze_personal_color 메서드 비동기 처리

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 from fastapi import FastAPI, UploadFile, File, Form
+from starlette.concurrency import run_in_threadpool
+
 from color_analysis_mediapipe import analyze_personal_color
 import uvicorn
 
 app = FastAPI()
 
 
+# @app.post("/analyze-color")
+# async def analyze_color(user_id: int = Form(...),
+#                         file: UploadFile = File(...)):
+#     contents = await file.read()
+#     result = analyze_personal_color(contents, user_id)
+#     return {"userId": user_id, "personalColor": result}
 @app.post("/analyze-color")
 async def analyze_color(user_id: int = Form(...),
                         file: UploadFile = File(...)):
     contents = await file.read()
-    result = analyze_personal_color(contents, user_id)
+    result = await run_in_threadpool(analyze_personal_color, contents, user_id)
     return {"userId": user_id, "personalColor": result}
-
 
 # PyCharm 실행 시
 if __name__ == "__main__":
