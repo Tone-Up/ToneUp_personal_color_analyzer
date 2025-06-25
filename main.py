5463f427e9a41a2aa0d3e4b60a8cac93f4ba36f4
김승용 <gkstjeo17@naver.com>
2025-06-25 17:28:28 +0900

    refactor: 동시 요청 처리 수 조절 위한 세마포어 적용

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import asyncio
 import time
 
 from fastapi import FastAPI, UploadFile, File, Form, HTTPException
@@ -20,17 +21,20 @@ app = FastAPI()
 @app.post("/analyze-color")
 async def analyze_color(user_id: int = Form(...),
                         file: UploadFile = File(...)):
-    start = time.monotonic()
-    try:
-        contents = await file.read()
-        result = await run_in_threadpool(analyze_personal_color, contents, user_id)
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-    finally:
-        elapsed = time.monotonic() - start
-        print(f"[analyze_color] 실행 시간: {elapsed:.3f}초")
+    semaphore = asyncio.Semaphore(12)  # 최대 5개 동시 요청 허용
 
-    return {"userId": user_id, "personalColor": result}
+    start = time.monotonic()
+    async with semaphore:
+        try:
+            contents = await file.read()
+            result = await run_in_threadpool(analyze_personal_color, contents, user_id)
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=str(e))
+        finally:
+            elapsed = time.monotonic() - start
+            print(f"[analyze_color] 실행 시간: {elapsed:.3f}초")
+
+        return {"userId": user_id, "personalColor": result}
 # PyCharm 실행 시
 if __name__ == "__main__":
     uvicorn.run("main:app", host="localhost", port=8000, reload=True)
